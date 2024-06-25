f4597268f2e66b2905211fdaba6c8afbebbfacfd
Pawel Trela <pawel.trela@fingoweb.com>
2024-06-25 14:05:37 +0200

    fix: added dlapi prefix to session storage keys

--- src/types/templates.ts
+++ src/types/templates.ts
@@ -1,9 +1,10 @@
+import { DL_API_PREFIX_KEY } from 'consts/common';
 import { EProductElements } from 'types/products';
 
 enum ETemplates {
-  LIST_VIEW = 'LIST_VIEW',
-  GRID_VIEW = 'GRID_VIEW',
-  LIST_RELATED_PRODUCTS = 'LIST_RELATED_PRODUCTS',
+  LIST_VIEW = `${DL_API_PREFIX_KEY}LIST_VIEW`,
+  GRID_VIEW = `${DL_API_PREFIX_KEY}GRID_VIEW`,
+  LIST_RELATED_PRODUCTS = `${DL_API_PREFIX_KEY}LIST_RELATED_PRODUCTS`,
 }
 
 type TContentMap = {
